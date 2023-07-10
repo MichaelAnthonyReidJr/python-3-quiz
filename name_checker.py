8bb7471d34caf662137ca31500d34add9dd2a502
MichaelAnthonyReidJr <EfficiencyMarriesEfficacy@gmail.com>
2023-07-10 10:44:35 -0400

    Add

--- name_checker.py
+++ name_checker.py
@@ -1,9 +1,20 @@
 from ValidationException import ValidationException
-
+#Create a function called validate_file() (in name_checker.py) which 
+# accepts a name of a file to validate. 
+# This function validates the users.txt file and checks that the 
+# first name for each user does not contain any numbers (0123456789). 
+# This function raises (throws) a ValidationException object and is 
+# consumed in the following manner:
 def validate_file(file_path):
-    pass #TODO: Only add code inside this function.
+    with open(file_path, 'r') as file:
+        for line in file:
+            if line.find("0123456789") != -1:
+                # print(line)
+                raise ValidationException("The first name cannot contain a number")
+            else:
+                print("The first name is valid")
 
-
+        
 def test():
     try:
         validate_file("users.txt")
